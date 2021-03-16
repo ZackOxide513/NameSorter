625d0ee01caf9571d1dc6d0f5fef8163e3ac084e
Vu Truong <hoangvutruong27894@gmail.com>
2021-03-16 21:11:35 +0800

    Added Person Data Model

---
+++ NameSorter/DataModel/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter.DataModel
+{
+    class Person
+    {
+        string GivenNames { get; set; }
+        string LastName { get; set; }
+    }
+}
