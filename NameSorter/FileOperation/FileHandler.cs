83700644c09ef04a428de7a9d0ecc7d3bb822f93
Vu Truong <hoangvutruong27894@gmail.com>
2021-03-16 20:07:10 +0800

    Created FileHandler to read txt

---
+++ NameSorter/FileOperation/FileHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter.FileOperation
+{
+    public class FileHandler
+    {
+        private string line;
+        private int counter;
+
+        public FileHandler()
+        {
+            line = "";
+            counter = 0;
+        }
+
+        public IList<string> ReadFromFile(string filePath)
+        {
+            IList<string> result = new List<string>();
+            
+            StreamReader file = new StreamReader(filePath);
+            while((line = file.ReadLine()) != null)
+            {
+                result.Add(line);
+                counter++;
+            }
+
+            file.Close();
+            return result;
+        }
+    }
+}
