5129c9aa0ee760120e8048e41b5c3ee48c38c9d7
Vu Truong <hoangvutruong27894@gmail.com>
2021-03-16 20:39:04 +0800

    Add dependency container

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Autofac;
+using NameSorter.FileOperation;
+using System;
 
 namespace NameSorter
 {
     class Program
     {
+        private static IContainer _container;
+        static Program()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<FileHandler>().SingleInstance();
+            _container = builder.Build();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileHandler = _container.Resolve<FileHandler>();
+            var result = fileHandler.ReadFromFile("namelist.txt");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.ReadLine();
         }
     }
 }
